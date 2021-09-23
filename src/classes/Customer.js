5a5fc5188206f32734518ee4f9b397bda733cdd8
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-09-23 08:25:15 -0600

    Complete Customer class using test suite

---
+++ src/classes/Customer.js
@@ -0,0 +1,66 @@
+import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+
+dayjs.extend(isBetween)
+
+class Customer {
+  constructor(customer) {
+    this.id = customer.id;
+    this.name = customer.name;
+    this.bookings = [];
+  }
+
+  getBookings(bookings) {
+    this.bookings = bookings.filter(booking => booking.userID === this.id)
+  }
+
+  getPastBookings() {
+    const today = dayjs();
+
+    const pastBookings = this.bookings.filter(booking => {
+      const checkOut = dayjs(booking.checkInDate)
+      .add(booking.duration, 'day');
+
+      return checkOut.isBefore(today);
+    })
+
+    if (pastBookings.length) {
+      return pastBookings;
+    } else {
+      return 'We couldn\'t find any past bookings.'
+    }
+  }
+
+  getPresentBookings() {
+    const today = dayjs();
+
+    const presentBookings = this.bookings.filter(booking => {
+      const checkOut = dayjs(booking.checkInDate)
+      .add(booking.duration, 'day')
+
+      return today.isBetween(booking.checkInDate, checkOut)
+    })
+
+    if (presentBookings.length) {
+      return presentBookings
+    } else {
+      return 'We couldn\'t find any present bookings.';
+    }
+  }
+
+  getUpcomingBookings() {
+    const today = dayjs();
+
+    const upcomingBookings = this.bookings.filter(booking => {
+      return dayjs(booking.checkInDate).isAfter(today);
+    })
+
+    if (upcomingBookings.length) {
+      return upcomingBookings;
+    } else {
+      return 'We couldn\'t find any upcoming bookings. Book your next adventure!';
+    }
+  }
+}
+
+export default Customer;
