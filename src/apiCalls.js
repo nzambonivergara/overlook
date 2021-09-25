f52570509744623d7d927d8c613db89d30566fd2
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-09-25 12:59:15 -0600

    Create fetch functions to load rooms, bookings, and customers data

---
+++ src/apiCalls.js
@@ -0,0 +1,54 @@
+const apiEndpoint = 'http://localhost:3001';
+
+function loadRooms() {
+  return fetch(`${apiEndpoint}/api/v1/rooms`)
+    .then(response => response.json())
+    .then(data => data.rooms)
+    .catch(error => console.error(error))
+}
+
+function loadAllCustomers() {
+  return fetch(`${apiEndpoint}/api/v1/customers`)
+    .then(response => response.json())
+    .then(data => data.customers)
+    .catch(error => console.error(error))
+}
+
+function loadSingleCustomer(customerID) {
+  return fetch(`${apiEndpoint}/api/v1/customers/${customerID}`)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.error(error))
+}
+
+function loadBookings() {
+  return fetch(`${apiEndpoint}/api/v1/bookings`)
+    .then(response => response.json())
+    .then(data => data.bookings)
+    .catch(error => console.log(error))
+}
+
+function addNewBookings(userId, date, roomNumber) {
+  return fetch(`${apiEndpoint}/api/v1/bookings`, {
+    method: 'POST',
+    body: JSON.stringify({
+      "userID": userId,
+      "date": date,
+      "roomNumber": roomNumber
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => data)
+  .catch(error => console.log(error))
+}
+
+export {
+  loadRooms,
+  loadAllCustomers,
+  loadSingleCustumer,
+  loadBookings,
+  addNewBookings
+}
